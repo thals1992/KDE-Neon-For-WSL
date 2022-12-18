d1a83bcca666389a65f11c50b2c4cab776657356
Trevor Halsey <thals1992@hotmail.com>
2022-12-18 13:06:15 -0500

    Self-elevate script
    used example from https://stackoverflow.com/questions/5944180/how-do-you-run-a-command-as-an-administrator-from-the-windows-command-line

--- kWSL.cmd
+++ kWSL.cmd
@@ -1,4 +1,27 @@
-@ECHO OFF & NET SESSION >NUL 2>&1 
+@ECHO OFF 
+<!-- : --- Self-Elevating Batch Script ---------------------------
+@whoami /groups | find "S-1-16-12288" > nul && goto :admin
+set "ELEVATE_CMDLINE=cd /d "%~dp0" & call "%~f0" %*"
+cscript //nologo "%~f0?.wsf" //job:Elevate & exit /b
+
+-->
+<job id="Elevate"><script language="VBScript">
+  Set objShell = CreateObject("Shell.Application")
+  Set objWshShell = WScript.CreateObject("WScript.Shell")
+  Set objWshProcessEnv = objWshShell.Environment("PROCESS")
+  strCommandLine = Trim(objWshProcessEnv("ELEVATE_CMDLINE"))
+  objShell.ShellExecute "cmd", "/c " & strCommandLine, "", "runas"
+</script></job>
+:admin -----------------------------------------------------------
+
+@echo off
+echo Running as elevated user.
+echo Script file : %~f0
+echo Arguments   : %*
+echo Working dir : %cd%
+echo.
+
+& NET SESSION >NUL 2>&1
 IF %ERRORLEVEL% == 0 (ECHO Administrator check passed...) ELSE (ECHO You need to run this command with administrative rights.  Is User Account Control enabled? && pause && goto ENDSCRIPT)
 COLOR 1F
 SET GITORG=thals1992
